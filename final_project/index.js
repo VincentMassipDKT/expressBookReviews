5eed4b673d3577c0c587929780669f205cd49c0a
VincentMassipDKT <vincent.massip@decathlon.com>
2026-09-14 17:45:54 +0200

    Adding authentication method ang global error handling

--- final_project/index.js
+++ final_project/index.js
@@ -5,18 +5,35 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: `Internal Server Error \n ${err.name} \n ${err.message}` });
 });
- 
-const PORT =5000;
+
+app.use("/customer", session({secret: "fingerprint_customer", resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req, res, next){
+    // Check if user is logged in and has valid access token
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "Forbidden" });
+            }
+        });
+    } else {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+});
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+const PORT = 5000;
+app.listen(PORT, () => console.log("Server is running"));
