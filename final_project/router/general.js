2ac7ea7d4112daa596de5aca39aff8c66fd1e0c9
VincentMassipDKT <vincent.massip@decathlon.com>
2026-09-14 18:24:14 +0200

    Task 3: Adding GET /author router

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,43 +5,48 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   const bookList = JSON.stringify(Object.values(books), null, 2);
   console.log(bookList);
   return res.status(200).json(bookList);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
   if (book) {
     return res.status(200).json(book);
   } else {
     return res.status(404).json({ message: `No book found for ISBN [ ${isbn} ]` });
-  }
+  };
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: `No books found for author [ ${author} ]` });
+  };
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
