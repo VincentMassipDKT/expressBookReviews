2ed3dd6fa3d07802c20899a4f9367c989f45c9f2
VincentMassipDKT <vincent.massip@decathlon.com>
2026-09-16 11:52:15 +0200

    Task 8: Adding PUT review/ router

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -19,7 +19,7 @@ const authenticatedUser = (username, password) => { //returns boolean
 };
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
   const username = req?.body?.username;
   const password = req?.body?.password;
@@ -31,19 +31,51 @@ regd_users.post("/login", (req,res) => {
   
   if (authenticatedUser(username, password)) {
     // Generate JWT token for the authenticated user
-    const token = jwt.sign({ data: password }, "access", { expiresIn: 60 * 60 });
+    let accessToken = jwt.sign({ data: password }, "access", { expiresIn: 60 * 60 });
     // Store access token and username in the session
-    req.session.authorization = { token, username };
+    req.session.authorization = { accessToken, username };
+
     return res.status(200).json({ message: `User [ ${username} ] logged in successfully` });
   } else {
     return res.status(401).json({ message: "Invalid username or password" });
   };
 });
 
+
+
+regd_users.get("/auth", (req, res) => {
+  const username = req.session?.authorization?.username;
+  const accessToken = req.session?.authorization?.accessToken;
+  
+  if (!username || !accessToken) {
+    return res.status(401).json({ message: "User isn't logged in" });
+  }
+  return res.status(200).json({ message: `User [ ${username} ] is authenticated with token [ ${accessToken} ]` });
+});
+
+
+
+
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params?.isbn;
+  const review = req?.body?.review;
+  const username = req.session?.authorization?.username;
+
+  if (!username) {
+    return res.status(401).json({ message: "User isn't logged in" });
+  };
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: `No book found for ISBN [ ${isbn} ]` });
+  };
+
+  if (!review) {
+    return res.status(400).json({ message: "Review content is missing" });
+  }
+
+  books[isbn].reviews[username] = review;
+  return res.status(200).json({ message: `Review successfully added/updated by [ ${username} ] for ISBN [ ${isbn} ]` });
 });
 
 module.exports.authenticated = regd_users;
